77add392e3cc2ea8b3e02d4d5d4549404d1765d2
ShaniRachmani <shani00111.rachmani@gmail.com>
2024-03-09 16:52:44 +0200

    My projects

---
+++ finalTar5/menu.js
@@ -0,0 +1,79 @@
+﻿menu = {
+    appetizers: [
+        {
+            name: "Greek Salad",
+            description: "Fresh salad with feta cheese and olive oil",
+            price: 30
+        },
+        {
+            name: "Tomato Soup",
+            description: "Excellent Italian tomato soup with croutons",
+            price: 35
+        },
+        {
+            name: "Caprese",
+            description: "Classic Italian dish with tomato sauce and mozzarella cheese",
+            price: 40
+        },
+        {
+            name: "Pizza Quiche",
+            description: "French quiche with a pizza twist, topped with tomato sauce and mozzarella cheese",
+            price: 45
+        },
+        {
+            name: "Fish Tacos",
+            description: "Fresh tacos with grilled fish slices and guava sauce",
+            price: 50
+        }
+    ],
+    main: [
+        {
+            name: "Antique Steak",
+            description: "Fresh steak with special sauce and choice of side dishes",
+            price: 100
+        },
+        {
+            name: "Alfredo Pasta",
+            description: "Italian pasta with creamy parmesan sauce",
+            price: 80
+        },
+        {
+            name: "Valentine's Sushi",
+            description: "Pan-fried tuna fillet with soy sauce and avocado",
+            price: 90
+        },
+        {
+            name: "Seafood Couscous",
+            description: "Couscous with Mediterranean seafood broth and fresh crackers",
+            price: 75
+        },
+        {
+            name: "The Burger",
+            description: "The original burger with a little extra",
+            price: 95
+        }
+    ],
+    extras: [
+        {
+            name: "sparkling water",
+            price: 12
+        },
+        {
+            name: "Pita Bread",
+            price: 8
+        },
+        {
+            name: "Fried Apples",
+            price: 15
+        },
+        {
+            name: "Espresso Coffee",
+            price: 18
+        },
+        {
+            name: "Strawberry Shake",
+            price: 20
+        }
+    ]
+};
+
